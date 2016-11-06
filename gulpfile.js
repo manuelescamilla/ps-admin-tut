7a9a3e05bf356a60b0fd8087738691c4ceee5a14
Manuel Escamilla <manuel.escamilla@teamaol.com>
2016-11-06 15:17:27 -0500

    configured browserify to handle js files

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // Runs a local server
 var open = require('gulp-open'); // Opens a url in the web server
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text stream with Gulp
 
 var config = {
     port: 9005,
     devBaseUrl: 'http:localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 }
 
@@ -23,7 +28,6 @@ gulp.task('connect', function(){
     });
 });
 
-
 gulp.task('open', ['connect'], function(){
     gulp.src('dist/index.html')
         .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
@@ -35,8 +39,19 @@ gulp.task('html', function(){
         .pipe(connect.reload());
 });
 
+gulp.task('js', function(){
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+})
+
 gulp.task('watch', function(){
-   gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
